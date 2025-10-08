f6bff22dab1fd16e0d895c8bcdddfa2e9ac979a9
cibieshs@gmail.com <cibieshs@gmail.com>
2025-10-08 18:45:49 +0530

    feat:added image in home page

--- backup/original-script.js
+++
@@ -1,1 +0,0 @@
-// original script.js backup
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import bgImage from '../assets/bg.jpg'
 
 export default function Home() {
   return (
     <div id="index-page">
       <header>
-        <img src="https://placehold.co/1200x300?text=Saranathan+College+of+Engineering+%7C+Excellence+in+Education" alt="College Banner" />
+        <img src={bgImage} alt="Saranathan College of Engineering - Excellence in Education" />
       </header>
       <main>
         <h1>Welcome to the College Portal</h1>
         <p>Your comprehensive gateway to academic excellence, resources, and campus life. Join our community of learners and innovators.</p>
         <div className="buttons">
           <Link to="/login" className="btn">Sign In</Link>
-          <Link to="/register" className="btn btn-secondary">Get Started</Link>
+          <Link to="/register" className="btn btn-secondary">Signup</Link>
         </div>
       </main>
     </div>
